82efcd26b467576334966c7964e7016f8ac9e117
Maurice Collier <mauricecollier84@yahoo.com>
2021-09-22 00:02:45 -0400

    Added the Bonus section.

--- script.js
+++ script.js
@@ -25,3 +25,22 @@ console.log(blank)
 // 9
 blank = null
 
+// Bonus
+
+// 1
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
+
+// 2
+console.log (quarter, dime, nickel, penny);
+
+// 3
+let letters = "qwertyuiop";
+
+// 4
+console.log(letters.indexOf("p"));
+
+
+
+
+
+
